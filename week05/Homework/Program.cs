b2b0ff4540c88df31d835a2a0f346e36eccd842c
Doris Nnenna Amuji <dorisnnenna2016@gmail.com>
2026-04-06 10:30:00 +0100

    homework add

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment("John Doe", "Fractions", "7.3", "3-10, 20-21");
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,17 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {GetStudentName()}";
+    }
+
+    // We need access to student name → so we add a method in base class
+}
